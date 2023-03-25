5d6531910f367ad6f37a1e793d13f8d8b42c83f3
Kürşat Yıldırım <aynurkursat1999@hotmail.com>
2023-03-25 14:11:14 +0300

    Add auth module and login,jwt adn passwordhas methods

--- api/src/user/controller/user.controller.ts
+++ api/src/user/controller/user.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { User } from '../models/user.interface';
 import { UserService } from '../service/user.service';
 
@@ -10,8 +10,20 @@ export class UserController {
 
 
     @Post()
-    create(@Body()user: User): Observable<User> {
-        return this.userService.create(user);
+    create(@Body()user: User): Observable<User | Object> {
+        return this.userService.create(user).pipe(
+            map((user:User) => user),
+            catchError(err => of({error: err.messsage}))
+        )
+    }
+
+    @Post('login')
+    login(@Body() user: User): Observable<Object> {
+        return this.userService.login(user).pipe(
+            map((jwt:string) => {
+                return {access_token: jwt};
+            })
+        )
     }
 
     @Get(':id')
--- api/src/user/models/user.entity.ts
+++ api/src/user/models/user.entity.ts
@@ -1,4 +1,4 @@
-import{ Entity, PrimaryGeneratedColumn, Column} from 'typeorm'
+import{ Entity, PrimaryGeneratedColumn, Column, BeforeInsert} from 'typeorm'
 
 
 @Entity()
@@ -11,4 +11,15 @@ export class UserEntity {
 
     @Column({unique:true})
     username:string;
+
+    @Column({nullable: true})
+    email: string;
+
+    @Column({nullable: true})
+    password: string;
+
+    @BeforeInsert()
+    emailToLowerCase() {
+        this.email = this.email.toLowerCase();
+    }
 }
